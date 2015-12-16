689ef64ea40e9c03bc2e8fddda4284fb2e9633d0
Majid Hatami Aghdam <mjhatamy@gmail.com>
2015-12-16 10:33:02 -0800

    Initial Commit

---
+++ CERangeSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CERangeSlider
+//
+//  Created by Majid Hatami Aghdam on 12/16/15.
+//  Copyright © 2015 Majid Hatami Aghdam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CERangeSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CERangeSlider
+//
+//  Created by Majid Hatami Aghdam on 12/16/15.
+//  Copyright © 2015 Majid Hatami Aghdam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
